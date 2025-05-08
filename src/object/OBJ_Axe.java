e4a12fc01f0cdd530439174a00e1b232f8665054
BlueSkiesPirate <sebastianreyna128@gmail.com>
2025-05-08 14:24:08 -0400

    changes

---
+++ src/object/OBJ_Axe.java
@@ -0,0 +1,19 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Axe extends Entity{
+
+	public OBJ_Axe(GamePanel gp) {
+		super(gp);
+		type = type_axe;
+		name = "axe";
+		down1 = setup("/objects/Axe");
+		attackValue = 1;
+		description ="[" +name +"]\nA trusty Axe"; 
+		attackArea.width = 72;
+		attackArea.height = 36;
+	}
+
+}
